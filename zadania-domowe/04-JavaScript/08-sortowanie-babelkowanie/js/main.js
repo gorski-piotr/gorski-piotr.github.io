d2093771521e36c5807be2f65924e5adb136f62a
Piotr Gorski <gorskipiotr5@gmail.com>
2021-02-10 22:10:44 +0000

    sortowanie babelkowe

---
+++ zadania-domowe/04-JavaScript/08-sortowanie-babelkowanie/js/main.js
@@ -0,0 +1,16 @@
+function bubbleSort(array) {
+
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (array[j] > array[j + 1]) {
+                let temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return array;
+};
+
+let mojaTablica = [12, 67, 34, 23.01, 24, 2, 56, 8, 0x10, 23.02, 78, 34, 1e2, 45, 67, 98, 1];
+console.log(bubbleSort(mojaTablica));  
