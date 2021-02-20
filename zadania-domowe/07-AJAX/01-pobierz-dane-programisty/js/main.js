d104dad8ffd4fa2cc27b47f9acf779fe5a0e618c
Piotr Gorski <gorskipiotr5@gmail.com>
2021-02-20 23:21:20 +0000

    07-ajax-01-pobierz-dane-programisty finished

---
+++ zadania-domowe/07-AJAX/01-pobierz-dane-programisty/js/main.js
@@ -0,0 +1,52 @@
+// $(function() {
+//     console.log("jQuery test");
+
+//     //metoda get nie zamienila mi stringa na json?
+//     $.get("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function(data) {
+//         console.log("jQuery data test");
+//         console.log(data);
+//         console.log(data.imie);
+//         console.log(data.nazwisko);
+//         console.log(data.zawod);
+//         console.log(data.firma);
+//     });
+
+//     //metodagetJSON dziala git
+//     $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function(data) {
+//         console.log("jQuery data test");
+//         console.log(data);
+//         console.log(data.imie);
+//         console.log(data.nazwisko);
+//         console.log(data.zawod);
+//         console.log(data.firma);
+//     });
+// });
+
+let btnDownloadData = document.getElementById("download-data");
+let divDaneProgramisty = document.getElementById("dane-programisty");
+
+const downloadData = () => {
+    $(function() {
+        $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function(data) {
+            console.log(data);
+            divDaneProgramisty.innerText = `Imie: ${data.imie},\nNazwisko: ${data.nazwisko},\nZawód: ${data.zawod},\nFirma: ${data.firma}.`
+            //divDaneProgramisty.textContent = `Imie: ${data.imie},\nNazwisko: ${data.nazwisko},\nZawód: ${data.zawod},\nFirma: ${data.firma}.`
+            //$("#dane-programisty").text(`Imie: ${data.imie},\nNazwisko: ${data.nazwisko},\nZawód: ${data.zawod},\nFirma: ${data.firma}.`);
+            // document.getElementById("dane-programisty").innerText = `Imie: ${data.imie},\nNazwisko: ${data.nazwisko},\nZawód: ${data.zawod},\nFirma: ${data.firma}.`
+        });
+    });
+}
+
+btnDownloadData = document.addEventListener("click", downloadData);
+
+//  document.getElementById("download-data").addEventListener("click", downloadData);
+
+
+//BEZ UZYCIA ZMIENNYCH (NAJMNEJ KODU, NAJMNIEJ CZYTELNE):
+// document.getElementById("download-data").addEventListener("click", function() {
+//     $(function() {
+//         $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function(data) {
+//             document.getElementById("dane-programisty").innerText = `Imie: ${data.imie},\nNazwisko: ${data.nazwisko},\nZawód: ${data.zawod},\nFirma: ${data.firma}.`
+//         });
+//     });
+// });
