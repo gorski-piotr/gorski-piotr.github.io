c25bdd8055de34d092d98e1de34c8b2199eea21b
Piotr Gorski <gorskipiotr5@gmail.com>
2021-02-21 09:26:35 +0000

    07-ajax-02-pobierz-dane-po-kliknieciu zadanie zrobione i wklejony kod z warsztatu

---
+++ zadania-domowe/07-AJAX/02-pobierz-dane-po-kliknieciu-przycisku/js/script.js
@@ -0,0 +1,26 @@
+let btnGetData = document.getElementById("btn-get-data");
+let divPrintData = document.getElementById("div-print-data");
+
+const getData = () => {
+    console.log("getData test")
+    fetch(`https://akademia108.pl/api/ajax/get-post.php`, {
+        mode: 'cors', // it is DEFAULT VALUE
+        method: 'GET', // allowed
+        // method: 'POST', // allowed by CORS, but NOT allowed by SERVER
+        // method: 'PUT', // NOT allowed on server by CORS
+      })
+        .then(res => res.json())
+        .then(resJSON => {
+            console.log(resJSON);
+            divPrintData.innerText = 
+            "userID = " + resJSON.userId + "\n" +
+            "id = " + resJSON.id + "\n" +
+            "title = " + resJSON.title + "\n" +
+            "body = " + resJSON.body
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+
+btnGetData = document.addEventListener("click", getData);
