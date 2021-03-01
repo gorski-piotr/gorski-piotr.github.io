7e77a7ac747b8e6bd177376b51c6b56c94019e63
Piotr Gorski <gorskipiotr5@gmail.com>
2021-03-01 18:02:17 +0000

    navigation in progress (without watching video) - works good so far, mobile-menu needs styling

--- project-spa/js/main.js
+++ project-spa/js/main.js
@@ -1,1 +1,10 @@
-console.log("test")
+
+
+
+// CREATING MENU FOR MOBILE PHONES:
+let menuButton = document.getElementById('menu-button');
+menuButton.addEventListener('click', function() {
+    console.log('menu-button clicked');
+    //let nav = document.getElementById('nav');
+    nav.classList.toggle('open');
+});
